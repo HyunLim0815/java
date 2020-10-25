6a1d9ce16288f280b7f4869288f7c48401da587c
Hyun_Lim0815 <2837440991@qq.com>
2020-10-25 21:33:37 +0800

    Practises of Introduction to Java Programming and Data Structures

---
+++ Character1/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ *  打印HelloWorld
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
---
+++ Character1/WelcomeToJava.java
@@ -0,0 +1,9 @@
+/**
+ * WelcomeToJava
+ */
+public class WelcomeToJava {
+
+    public static void main(String[] args) {
+        System.out.print("Welcome to Java");
+    }
+}
